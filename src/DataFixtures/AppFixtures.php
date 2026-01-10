4d3540469f9eb30c3821defee236259a05a2b0d9
Markel Alvarado <markel.alvarado@gmail.com>
2026-01-10 14:06:32 +0100

    Carga de Fixtures para tipos de receta y nutrientes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,7 +1,8 @@
-<?php
-
+// src/DataFixtures/AppFixtures.php
 namespace App\DataFixtures;
 
+use App\Entity\NutrientType;
+use App\Entity\RecipeType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,31 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // 1. Tipos de Receta
+        $types = [
+            ['Postre', 'Para endulzar un buen menú'],
+            ['Ensalada', 'Sanas y frescas'],
+            ['Carne', 'Proteína pura'],
+            ['Potaje', 'Platos de cuchara']
+        ];
+        foreach ($types as $t) {
+            $type = new RecipeType();
+            $type->setName($t[0])->setDescription($t[1]);
+            $manager->persist($type);
+        }
+
+        // 2. Nutrientes
+        $nutrients = [
+            ['Proteinas', 'gr'],
+            ['Grasas', 'gr'],
+            ['Carbohidratos', 'gr'],
+            ['Calorías', 'kcal']
+        ];
+        foreach ($nutrients as $n) {
+            $nut = new NutrientType();
+            $nut->setName($n[0])->setUnit($n[1]);
+            $manager->persist($nut);
+        }
 
         $manager->flush();
     }
